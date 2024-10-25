dbb126ef164f0890f3ce57eaefe3f3ec12b49245
Divya Kohli <73181773+DivyaKohli@users.noreply.github.com>
2024-10-25 21:08:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Auto scroll
+document.body.style.overflowX = "hidden";
+
 // toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
@@ -12,13 +15,13 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec =>{
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -52,12 +55,20 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Data Analyst', 'Tableau Developer', 'Power BI Developer'],
+    strings: ['Data Analyst', 'Business Analyst', 'Developer'],
     typeSpeed: 100,
     backSPeed: 100,
     backDelay: 1000,
     loop: true
 });
+
+// Reset form
+// document.querySelector("form").addEventListener("reset", function () {
+//     for (var i = 0; i < this.elements.length; i++) {
+//         this.elements[i].value = "";
+//     }
+// });
+
 
 // Read more
 
